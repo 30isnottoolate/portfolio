247819f466ff3816de8990313960da7e9676523a
30isnottoolate <akos.varga.92@gmail.com>
2022-10-09 15:38:21 +0200

    Add links to page sections

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -5,14 +5,19 @@ function Portfolio() {
 	return (
 		<div id="portfolio">
 			<header>
+				<a id="logo" href="#welcome">30isnottoolate</a>
 				<nav>
+					<a href="#bio">Bio</a>
+					<a href="#skills">Skills</a>
+					<a href="#works">Portfolio</a>
+					<a href="#contact">Contact</a>
 				</nav>
 			</header>
 			<main>
 				<section id="welcome"></section>
 				<section id="bio"></section>
 				<section id="skills"></section>
-				<section id="portfolio"></section>
+				<section id="works"></section>
 				<section id="contact"></section>
 			</main>
 			<footer id="bottom"></footer>
