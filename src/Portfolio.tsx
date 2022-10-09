d2a8629b081922347c1fe687b1e0adbc0843bc86
30isnottoolate <akos.varga.92@gmail.com>
2022-10-09 15:16:24 +0200

    Add basic structure to page

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -4,7 +4,18 @@ import './Portfolio.css';
 function Portfolio() {
 	return (
 		<div id="portfolio">
-			Welcome to my portfolio page!
+			<header>
+				<nav>
+				</nav>
+			</header>
+			<main>
+				<section id="welcome"></section>
+				<section id="bio"></section>
+				<section id="skills"></section>
+				<section id="portfolio"></section>
+				<section id="contact"></section>
+			</main>
+			<footer id="bottom"></footer>
 		</div>
 	);
 }
