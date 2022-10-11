9c6ab50363ee3d5144db84934bcd2b333b37ea95
30isnottoolate <akos.varga.92@gmail.com>
2022-10-11 18:22:23 +0200

    Populate #skills section with links and images

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -26,7 +26,50 @@ function Portfolio() {
 					</h2>
 				</section>
 				<section id="bio"></section>
-				<section id="skills"></section>
+				<section id="skills">
+					<h1>My Skills</h1>
+					<h2>Languages</h2>
+					<div id="langs">
+						<a href="https://html.spec.whatwg.org/" target="_blank">
+							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://www.w3.org/TR/CSS/" target="_blank">
+							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://www.ecma-international.org/publications-and-standards/standards/ecma-262/" target="_blank">
+							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://www.typescriptlang.org/" target="_blank">
+							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+					</div>
+					<h2>Technologies and frameworks</h2>
+					<div id="techs">
+						<a href="https://reactjs.org/" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://nodejs.org/" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://expressjs.com/" target="_blank"><img src="https://expressjs.com/images/favicon.png" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://www.mongodb.com/" target="_blank"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+					</div>
+					<h2>Tools</h2>
+					<div id="tools">
+						<a href="https://git-scm.com/" target="_blank">
+							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://code.visualstudio.com/" target="_blank">
+							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://www.gimp.org/" target="_blank">
+							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gimp/gimp-original.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+						<a href="https://www.adobe.com/products/photoshop.html" target="_blank">
+							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg" style={{ height: "40px", width: "40px", padding: "5px" }} />
+						</a>
+					</div>
+				</section>
 				<section id="works"></section>
 				<section id="contact"></section>
 			</main>
