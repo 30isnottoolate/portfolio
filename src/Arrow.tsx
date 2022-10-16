6811059cf549958abf310791ade094b0220529bb
30isnottoolate <akos.varga.92@gmail.com>
2022-10-16 12:19:26 +0200

    Add fill style to component

--- src/Arrow.tsx
+++ src/Arrow.tsx
@@ -10,7 +10,7 @@ interface ArrowProps {
 
 const Arrow: React.FC<ArrowProps> = ({arrowClassName, posX, posY, mirrored, clickHandler, color, strokeWidth}: ArrowProps) => {
     return (
-        <svg className={`arrow-button ${arrowClassName}`} style={{ left: posX, top: posY, transform: mirrored ? "scaleX(-1)" : "scaleX(1)" }} onClick={clickHandler} viewBox="-5 -5 35 60">
+        <svg className={`arrow-button ${arrowClassName}`} style={{ fill: "#000f20", left: posX, top: posY, transform: mirrored ? "scaleX(-1)" : "scaleX(1)" }} onClick={clickHandler} viewBox="-5 -5 35 60">
             <polygon points="0,0 25,25 0,50" style={{ stroke: color, strokeWidth: strokeWidth }} />
         </svg>
     );
