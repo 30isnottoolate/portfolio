54fb4c8865abe438a75bf026962c1a108cfd65c4
30isnottoolate <akos.varga.92@gmail.com>
2022-10-18 16:07:42 +0200

    Create new component

---
+++ src/MenuButton.tsx
@@ -0,0 +1,11 @@
+const MenuButton = () => {
+    return (
+        <div className="menu-button" >
+            <div style={{width: 35, height: 5, borderRadius: 2.5, margin: "5px 0"}} />
+            <div style={{width: 35, height: 5, borderRadius: 2.5, margin: "5px 0"}}/>
+            <div style={{width: 35, height: 5, borderRadius: 2.5, margin: "5px 0"}}/>
+        </div>
+    );
+}
+
+export default MenuButton;
