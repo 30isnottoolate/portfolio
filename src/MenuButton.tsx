84eae7a4bbd37337cabaa570988bba1b072cf977
30isnottoolate <akos.varga.92@gmail.com>
2022-10-18 17:54:28 +0200

    Finetune menu button elements animation

--- src/MenuButton.tsx
+++ src/MenuButton.tsx
@@ -6,9 +6,9 @@ interface MenuButtonProps {
 const MenuButton = ({isMenuVisible, clickHandler}: MenuButtonProps) => {
     return (
         <div className="menu-button" style={{cursor: "pointer"}} onClick={clickHandler} >
-            <div style={{transform: (isMenuVisible ? "translate(0, 11px) rotate(-45deg)" : "none")}} />
+            <div style={{transform: (isMenuVisible ? "translate(0, 10px) rotate(-45deg)" : "none")}} />
             <div style={{opacity: (isMenuVisible ? "0" : "1")}}/>
-            <div style={{transform: (isMenuVisible ? "translate(0, -11px) rotate(45deg)" : "none")}}/>
+            <div style={{transform: (isMenuVisible ? "translate(0, -10px) rotate(45deg)" : "none")}}/>
         </div>
     );
 }
