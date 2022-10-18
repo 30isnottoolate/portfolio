a74455c978b121a3d03ec9cb2e2dcb7161242b04
30isnottoolate <akos.varga.92@gmail.com>
2022-10-18 14:15:48 +0200

    Add optional background color prop to component

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -2,12 +2,13 @@ interface IconProps {
     href: string;
     src: string;
     alt: string;
+    bGColor?: string;
 }
 
-const Icon: React.FC<IconProps> = ({ href, src, alt }: IconProps) => {
+const Icon: React.FC<IconProps> = ({ href, src, alt, bGColor }: IconProps) => {
     return (
         <a href={href} target="_blank" rel="noreferrer">
-            <img className="icon-image" src={src} alt={alt} />
+            <img className="icon-image" src={src} alt={alt} style={{backgroundColor: bGColor}}/>
         </a>
     );
 }
