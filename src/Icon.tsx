3b335bcd2f2fcae34f694efd6e0c96086164cf83
30isnottoolate <akos.varga.92@gmail.com>
2022-10-19 23:40:07 +0200

    Add basic hover effect to component

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -6,9 +6,13 @@ interface IconProps {
 }
 
 const Icon: React.FC<IconProps> = ({ href, src, alt, bGColor }: IconProps) => {
+    const handleHover = () => {
+        console.log(alt);
+    }
+
     return (
         <a href={href} target="_blank" rel="noreferrer">
-            <img className="icon-image" src={src} alt={alt} style={{backgroundColor: bGColor}}/>
+            <img className="icon-image" src={src} alt={alt} style={{backgroundColor: bGColor}} onMouseEnter={handleHover}/>
         </a>
     );
 }
