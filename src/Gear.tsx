2b1e55a1d9a6e4ecb4586bd52fea6d0d95a4ee4a
30isnottoolate <akos.varga.92@gmail.com>
2022-10-23 23:52:11 +0200

    Create new component (Gear)

---
+++ src/Gear.tsx
@@ -0,0 +1,15 @@
+const Gear: React.FC<{viewportWidth: number, scrollPos: number}> = ({viewportWidth, scrollPos}) => {
+    return (
+        <img
+            id="gear"
+            src="./gear.svg"
+            alt="Gear"
+            style={{
+                left: `${viewportWidth <= 650 ? 4.5 : 5}px`,
+                top: `${viewportWidth <= 650 ? 4.5 : 5}px`,
+                height: `${viewportWidth <= 650 ? 36 : 45}px`,
+                transform: `rotate(${scrollPos / ((viewportWidth <= 650 ? 36 : 45) * Math.PI / 360)}deg)`
+            }}
+        />
+    );
+}
