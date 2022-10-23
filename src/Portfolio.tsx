f522a5e6e207858100f3d0eacd5f4ae8f66dff86
30isnottoolate <akos.varga.92@gmail.com>
2022-10-23 23:36:44 +0200

    Import BackToTop component

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Portfolio.css';
 import MenuButton from './MenuButton';
+import BackToTop from './BackToTop';
 import Icon from './Icon';
 import Project from './Project';
 import Arrow from './Arrow';
@@ -108,24 +109,7 @@ const Portfolio: React.FC = () => {
 					transform: `rotate(${scrollPos / ((viewportWidth <= 650 ? 36 : 45) * Math.PI / 360)}deg)`
 				}}
 			/>
-			<a
-				id="back-to-top"
-				href="#welcome"
-				style={{
-					position: "fixed",
-					bottom: (scrollPos > 1300 ? "30px" : "-50px"), right: "40px"
-				}}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="45"
-					height="45"
-					fill="#80b9ff"
-					stroke="#000f20"
-					strokeWidth="0.1"
-					viewBox="0 0 16 16">
-					<path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
-				</svg>
-			</a>
+			<BackToTop scrollPos={scrollPos}/>
 			<main>
 				<section id="welcome">
 					<img
