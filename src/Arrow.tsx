e7f9c57e628a242e9f0af7c722609a42d2d38ed0
30isnottoolate <akos.varga.92@gmail.com>
2022-10-25 13:53:14 +0200

    Reformat code

--- src/Arrow.tsx
+++ src/Arrow.tsx
@@ -10,10 +10,29 @@ interface ArrowProps {
     strokeWidth: number;
 }
 
-const Arrow: React.FC<ArrowProps> = ({arrowClassName, posX, posY, mirrored, clickHandler, color, hoverColor, bgColor, strokeWidth}: ArrowProps) => {
+const Arrow: React.FC<ArrowProps> = ({
+    arrowClassName, posX, posY, mirrored, clickHandler, color, hoverColor, bgColor, strokeWidth 
+}: ArrowProps) => {
     return (
-        <svg className={`arrow-button ${arrowClassName}`} style={{ fill: bgColor, left: posX, top: posY, transform: mirrored ? "scaleX(-1)" : "scaleX(1)" }} onClick={clickHandler} viewBox="-5 -5 35 60">
-            <polygon points="0,0 25,25 0,50" onMouseEnter={(event) => event.currentTarget.style.stroke = hoverColor} onMouseLeave={(event) => event.currentTarget.style.stroke = color} style={{ stroke: color, strokeWidth: strokeWidth }} />
+        <svg
+            className={`arrow-button ${arrowClassName}`}
+            style={{
+                fill: bgColor,
+                left: posX,
+                top: posY,
+                transform: mirrored ? "scaleX(-1)" : "scaleX(1)"
+            }}
+            onClick={clickHandler}
+            viewBox="-5 -5 35 60"
+        >
+            <polygon
+                points="0,0 25,25 0,50"
+                onMouseEnter={(event) => event.currentTarget.style.stroke = hoverColor}
+                onMouseLeave={(event) => event.currentTarget.style.stroke = color}
+                style={{
+                    stroke: color,
+                    strokeWidth: strokeWidth
+                }} />
         </svg>
     );
 }
