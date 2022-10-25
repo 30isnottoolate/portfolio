c49eddceac0a540734c1e957441c28089e9047fe
30isnottoolate <akos.varga.92@gmail.com>
2022-10-25 13:46:47 +0200

    Fix hover effect (replace fill with stroke)

--- src/Arrow.tsx
+++ src/Arrow.tsx
@@ -1,5 +1,3 @@
-import { hover } from "@testing-library/user-event/dist/hover";
-
 interface ArrowProps {
     arrowClassName: string;
     posX: number;
@@ -15,7 +13,7 @@ interface ArrowProps {
 const Arrow: React.FC<ArrowProps> = ({arrowClassName, posX, posY, mirrored, clickHandler, color, hoverColor, bgColor, strokeWidth}: ArrowProps) => {
     return (
         <svg className={`arrow-button ${arrowClassName}`} style={{ fill: bgColor, left: posX, top: posY, transform: mirrored ? "scaleX(-1)" : "scaleX(1)" }} onClick={clickHandler} viewBox="-5 -5 35 60">
-            <polygon points="0,0 25,25 0,50" onMouseEnter={(event) => event.currentTarget.style.stroke = hoverColor} onMouseLeave={(event) => event.currentTarget.style.fill = color} style={{ stroke: color, strokeWidth: strokeWidth }} />
+            <polygon points="0,0 25,25 0,50" onMouseEnter={(event) => event.currentTarget.style.stroke = hoverColor} onMouseLeave={(event) => event.currentTarget.style.stroke = color} style={{ stroke: color, strokeWidth: strokeWidth }} />
         </svg>
     );
 }
