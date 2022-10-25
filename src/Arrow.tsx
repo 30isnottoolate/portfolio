02dd11b9d7826eb114c4162f718891d2417fa252
30isnottoolate <akos.varga.92@gmail.com>
2022-10-25 13:44:33 +0200

    Add hoverColor and hover effect to component

--- src/Arrow.tsx
+++ src/Arrow.tsx
@@ -1,3 +1,5 @@
+import { hover } from "@testing-library/user-event/dist/hover";
+
 interface ArrowProps {
     arrowClassName: string;
     posX: number;
@@ -5,14 +7,15 @@ interface ArrowProps {
     mirrored: boolean;
     clickHandler: () => void;
     color: string;
+    hoverColor: string;
     bgColor: string;
     strokeWidth: number;
 }
 
-const Arrow: React.FC<ArrowProps> = ({arrowClassName, posX, posY, mirrored, clickHandler, color, bgColor, strokeWidth}: ArrowProps) => {
+const Arrow: React.FC<ArrowProps> = ({arrowClassName, posX, posY, mirrored, clickHandler, color, hoverColor, bgColor, strokeWidth}: ArrowProps) => {
     return (
         <svg className={`arrow-button ${arrowClassName}`} style={{ fill: bgColor, left: posX, top: posY, transform: mirrored ? "scaleX(-1)" : "scaleX(1)" }} onClick={clickHandler} viewBox="-5 -5 35 60">
-            <polygon points="0,0 25,25 0,50" style={{ stroke: color, strokeWidth: strokeWidth }} />
+            <polygon points="0,0 25,25 0,50" onMouseEnter={(event) => event.currentTarget.style.stroke = hoverColor} onMouseLeave={(event) => event.currentTarget.style.fill = color} style={{ stroke: color, strokeWidth: strokeWidth }} />
         </svg>
     );
 }
