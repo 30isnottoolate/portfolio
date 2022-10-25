e5ed23228e6f2e4d57ce5c9ce1146c5c44e3149c
30isnottoolate <akos.varga.92@gmail.com>
2022-10-25 13:40:56 +0200

    Add bgColor prop to Arrow

--- src/Arrow.tsx
+++ src/Arrow.tsx
@@ -5,12 +5,13 @@ interface ArrowProps {
     mirrored: boolean;
     clickHandler: () => void;
     color: string;
+    bgColor: string;
     strokeWidth: number;
 }
 
-const Arrow: React.FC<ArrowProps> = ({arrowClassName, posX, posY, mirrored, clickHandler, color, strokeWidth}: ArrowProps) => {
+const Arrow: React.FC<ArrowProps> = ({arrowClassName, posX, posY, mirrored, clickHandler, color, bgColor, strokeWidth}: ArrowProps) => {
     return (
-        <svg className={`arrow-button ${arrowClassName}`} style={{ fill: "#000f20", left: posX, top: posY, transform: mirrored ? "scaleX(-1)" : "scaleX(1)" }} onClick={clickHandler} viewBox="-5 -5 35 60">
+        <svg className={`arrow-button ${arrowClassName}`} style={{ fill: bgColor, left: posX, top: posY, transform: mirrored ? "scaleX(-1)" : "scaleX(1)" }} onClick={clickHandler} viewBox="-5 -5 35 60">
             <polygon points="0,0 25,25 0,50" style={{ stroke: color, strokeWidth: strokeWidth }} />
         </svg>
     );
