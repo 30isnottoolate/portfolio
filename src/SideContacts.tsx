0e5bb94ea8516a68f8e264a84a629b029c975667
30isnottoolate <akos.varga.92@gmail.com>
2022-11-02 00:11:19 +0100

    Rename contact-image classNames

--- src/SideContacts.tsx
+++ src/SideContacts.tsx
@@ -4,17 +4,17 @@ const SideContacts: React.FC = () => {
     return (
         <div id="side-contacts">
             <div className="side-contact">
-                <svg className="contact-image" viewBox="0 0 16 16">
+                <svg className="side-contact-image" viewBox="0 0 16 16">
                     <path d={contacts["linkedin"].path} />
                 </svg>
             </div>
             <div className="side-contact">
-                <svg className="contact-image" viewBox="0 0 16 16">
+                <svg className="side-contact-image" viewBox="0 2 16 12">
                     <path d={contacts["email"].path} />
                 </svg>
             </div>
             <div className="side-contact">
-                <svg className="contact-image" viewBox="0 0 16 16">
+                <svg className="side-contact-image" viewBox="0 0 16 16">
                     <path d={contacts["github"].path} />
                 </svg>
             </div>
