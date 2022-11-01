8cb89cc0ffbb5d7f64727380151fb128719f27c0
30isnottoolate <akos.varga.92@gmail.com>
2022-11-01 23:21:41 +0100

    Read displayText from contacts object

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -3,17 +3,16 @@ import contacts from './contacts';
 interface ContactProps {
     svgFillClass: string;
     contactType: string;
-    text: string;
 }
 
-const Contact: React.FC<ContactProps> = ({ svgFillClass, contactType, text }: ContactProps) => {
+const Contact: React.FC<ContactProps> = ({ svgFillClass, contactType}: ContactProps) => {
     return (
         <div>
             <a className={svgFillClass} href={contacts[contactType as keyof typeof contacts].href} target="_blank" rel="noreferrer">
                 <svg className="contact-image" viewBox="0 0 16 16">
                     <path d={contacts[contactType as keyof typeof contacts].path} />
                 </svg>
-                <span className="contact-text" >{text}</span>
+                <span className="contact-text" >{contacts[contactType as keyof typeof contacts].displayText}</span>
             </a>
         </div>
     );
