2aad1f6005b06b41faf4c628eea7810b931d1fc6
30isnottoolate <akos.varga.92@gmail.com>
2022-11-02 00:17:06 +0100

    Add themeClass prop to component

--- src/SideContacts.tsx
+++ src/SideContacts.tsx
@@ -1,19 +1,19 @@
 import contacts from "./contacts";
 
-const SideContacts: React.FC = () => {
+const SideContacts: React.FC<{sideContactTheme: string}> = (themeClass) => {
     return (
         <div id="side-contacts">
-            <div className="side-contact">
+            <div className={`side-contact ${themeClass}`} >
                 <svg className="side-contact-image" viewBox="0 0 16 16">
                     <path d={contacts["linkedin"].path} />
                 </svg>
             </div>
-            <div className="side-contact">
+            <div className={`side-contact ${themeClass}`} >
                 <svg className="side-contact-image" viewBox="0 2 16 12">
                     <path d={contacts["email"].path} />
                 </svg>
             </div>
-            <div className="side-contact">
+            <div className={`side-contact ${themeClass}`} >
                 <svg className="side-contact-image" viewBox="0 0 16 16">
                     <path d={contacts["github"].path} />
                 </svg>
