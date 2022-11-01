0592277dfcb35dd4b2a58db707f2429bc3aafdc0
30isnottoolate <akos.varga.92@gmail.com>
2022-11-01 23:36:56 +0100

    Replace className with id

--- src/MenuButton.tsx
+++ src/MenuButton.tsx
@@ -6,7 +6,7 @@ interface MenuButtonProps {
 
 const MenuButton = ({clickHandler, themeClass, isMenuVisible}: MenuButtonProps) => {
     return (
-        <div className="menu-button" style={{cursor: "pointer"}} onClick={clickHandler} >
+        <div id="menu-button" style={{cursor: "pointer"}} onClick={clickHandler} >
             <div className={themeClass} style={{transform: (isMenuVisible ? "translate(0, 10px) rotate(-45deg)" : "none")}} />
             <div className={themeClass} style={{opacity: (isMenuVisible ? "0" : "1")}}/>
             <div className={themeClass} style={{transform: (isMenuVisible ? "translate(0, -10px) rotate(45deg)" : "none")}}/>
