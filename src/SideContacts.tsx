ec873e90cc8c3c5fd636cfd51cde66ff7239c918
30isnottoolate <akos.varga.92@gmail.com>
2022-11-02 00:50:57 +0100

    Restructure component

--- src/SideContacts.tsx
+++ src/SideContacts.tsx
@@ -3,27 +3,27 @@ import contacts from "./contacts";
 const SideContacts: React.FC<{ themeClass: string }> = ({ themeClass }) => {
     return (
         <div id="side-contacts">
-            <div className={`side-contact ${themeClass}`} >
-                <a href={contacts["linkedin"].href} target="_blank" rel="noreferrer" >
+            <a href={contacts["linkedin"].href} target="_blank" rel="noreferrer" >
+                <div className={`side-contact ${themeClass}`} >
                     <svg className="side-contact-image" viewBox="0 0 16 16">
                         <path d={contacts["linkedin"].path} />
                     </svg>
-                </a>
-            </div>
-            <div className={`side-contact ${themeClass}`} >
-                <a href={contacts["email"].href} target="_blank" rel="noreferrer" >
+                </div>
+            </a>
+            <a href={contacts["email"].href} target="_blank" rel="noreferrer" >
+                <div className={`side-contact ${themeClass}`} >
                     <svg className="side-contact-image" viewBox="0 2 16 12">
                         <path d={contacts["email"].path} />
                     </svg>
-                </a>
-            </div>
-            <div className={`side-contact ${themeClass}`} >
-                <a href={contacts["github"].href} target="_blank" rel="noreferrer" >
+                </div>
+            </a>
+            <a href={contacts["github"].href} target="_blank" rel="noreferrer" >
+                <div className={`side-contact ${themeClass}`} >
                     <svg className="side-contact-image" viewBox="0 0 16 16">
                         <path d={contacts["github"].path} />
                     </svg>
-                </a>
-            </div>
+                </div>
+            </a>
         </div>
     )
 }
