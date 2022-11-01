fb9d29176b18602ed1b14256107172c860831b54
30isnottoolate <akos.varga.92@gmail.com>
2022-11-01 23:44:36 +0100

    Import contacts, add svg paths to divs

--- src/SideContacts.tsx
+++ src/SideContacts.tsx
@@ -1,9 +1,23 @@
+import contacts from "./contacts";
+
 const SideContacts: React.FC = () => {
     return (
         <div id="side-contacts">
-            <div className="side-contact"></div>
-            <div className="side-contact"></div>
-            <div className="side-contact"></div>
+            <div className="side-contact">
+                <svg className="contact-image" viewBox="0 0 16 16">
+                    <path d={contacts["linkedin"].path} />
+                </svg>
+            </div>
+            <div className="side-contact">
+                <svg className="contact-image" viewBox="0 0 16 16">
+                    <path d={contacts["email"].path} />
+                </svg>
+            </div>
+            <div className="side-contact">
+                <svg className="contact-image" viewBox="0 0 16 16">
+                    <path d={contacts["github"].path} />
+                </svg>
+            </div>
         </div>
     )
 }
