b0a527d60662801d64b0744c9668a4dbe4caf9f5
30isnottoolate <akos.varga.92@gmail.com>
2022-11-02 17:10:44 +0100

    Add scrollPos prop to component

--- src/SideContacts.tsx
+++ src/SideContacts.tsx
@@ -1,24 +1,24 @@
 import contacts from "./contacts";
 
-const SideContacts: React.FC<{ themeClass: string }> = ({ themeClass }) => {
+const SideContacts: React.FC<{ scrollPos: number, themeClass: string }> = ({ scrollPos, themeClass }) => {
     return (
         <div id="side-contacts">
             <a href={contacts["linkedin"].href} target="_blank" rel="noreferrer" >
-                <div className={`side-contact ${themeClass}`} >
+                <div className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1410 ? "-80px" : "-25px")}} >
                     <svg className="side-contact-image" viewBox="0 0 16 16">
                         <path d={contacts["linkedin"].path} />
                     </svg>
                 </div>
             </a>
             <a href={contacts["email"].href} target="_blank" rel="noreferrer" >
-                <div className={`side-contact ${themeClass}`} >
+                <div className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1350 ? "-80px" : "-25px")}} >
                     <svg className="side-contact-image" viewBox="0 2 16 12">
                         <path d={contacts["email"].path} />
                     </svg>
                 </div>
             </a>
             <a href={contacts["github"].href} target="_blank" rel="noreferrer" >
-                <div className={`side-contact ${themeClass}`} >
+                <div className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1300 ? "-80px" : "-25px")}} >
                     <svg className="side-contact-image" viewBox="0 0 16 16">
                         <path d={contacts["github"].path} />
                     </svg>
