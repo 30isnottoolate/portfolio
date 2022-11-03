7c12fc3dd5992d1ddafca04727637f495a1b9c24
30isnottoolate <akos.varga.92@gmail.com>
2022-11-03 12:45:34 +0100

    Restructure component

--- src/SideContacts.tsx
+++ src/SideContacts.tsx
@@ -2,27 +2,21 @@ import contacts from "./contacts";
 
 const SideContacts: React.FC<{ scrollPos: number, themeClass: string }> = ({ scrollPos, themeClass }) => {
     return (
-        <div id="side-contacts" style={{}}>
-            <a href={contacts["linkedin"].href} target="_blank" rel="noreferrer" >
-                <div id="side-contact-1" className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1410 ? "-80px" : "")}} >
+        <div id="side-contacts">
+            <a id="side-contact-1" className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1410 ? "-80px" : "")}} href={contacts["linkedin"].href} target="_blank" rel="noreferrer" >
                     <svg className="side-contact-image" viewBox="0 0 16 16">
                         <path d={contacts["linkedin"].path} />
                     </svg>
-                </div>
             </a>
-            <a href={contacts["email"].href} target="_blank" rel="noreferrer" >
-                <div id="side-contact-2" className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1350 ? "-80px" : "")}} >
+            <a id="side-contact-2" className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1350 ? "-80px" : "")}} href={contacts["email"].href} target="_blank" rel="noreferrer" >
                     <svg className="side-contact-image" viewBox="0 2 16 12">
                         <path d={contacts["email"].path} />
                     </svg>
-                </div>
             </a>
-            <a href={contacts["github"].href} target="_blank" rel="noreferrer" >
-                <div id="side-contact-3" className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1300 ? "-80px" : "")}} >
+            <a id="side-contact-3" className={`side-contact ${themeClass}`} style={{left: (scrollPos > 1300 ? "-80px" : "")}} href={contacts["github"].href} target="_blank" rel="noreferrer" >
                     <svg className="side-contact-image" viewBox="0 0 16 16">
                         <path d={contacts["github"].path} />
                     </svg>
-                </div>
             </a>
         </div>
     )
