8c1e31a5736043e90b384716da008c2c42d1d95e
30isnottoolate <akos.varga.92@gmail.com>
2022-11-09 16:57:09 +0100

    Put content into div container

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -126,7 +126,7 @@ const Portfolio: React.FC = () => {
 
 	return (
 		<div id="portfolio">
-			<Background 
+			<Background
 				themeClass={isItDark ? "dark-bg" : "light-bg"}
 			/>
 			<header className={headerFooterTheme()} >
@@ -183,250 +183,252 @@ const Portfolio: React.FC = () => {
 				svgFillClass={svgFillTheme()}
 				scrollPos={scrollPos}
 			/>
-			<main className={isItDark ? "dark-main" : "light-main"} >
-				<section id="welcome">
-					<img
-						id="avatar-image"
-						className={isItDark ? "dark-avatar" : "light-avatar"}
-						src="./images/avatar.png"
-						alt="Akos Varga"
-					/>
-					<h1 id="topH">
-						Hi! My name is <span>Akoš Varga</span>.
-					</h1>
-					<h2 id="middleH">
-						Welcome to my portfolio page!
-					</h2>
-					<h2>
-						I'm an aspiring <span>front-end developer</span>, constantly improving.
-					</h2>
-				</section>
-				<section id="bio">
-					<h1>About me</h1>
-					<ul>
-						<li>born and living in Serbia</li>
-						<li>self-taught <span>front-end developer</span>, designer</li>
-						<li>possessor of a barely used electronics technician degree</li>
-						<li><span>technology enthusiast</span></li>
-						<li><span>problem solver</span></li>
-						<li>heavy music enjoyer</li>
-						<li>bedroom guitar player</li>
-					</ul>
-				</section>
-				<section id="skills">
-					<h1>My Skills</h1>
-					<>
-						<h2>Languages</h2>
-						<div className="icon-collection" >
-							<Icon
-								href={"https://html.spec.whatwg.org/"}
-								svgFillClass={svgFillTheme()}
-								iconType="html"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://www.w3.org/TR/CSS/"}
-								svgFillClass={svgFillTheme()}
-								iconType="css"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://www.ecma-international.org/publications-and-standards/standards/ecma-262/"}
-								svgFillClass={svgFillTheme()}
-								iconType="js"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://www.typescriptlang.org/"}
-								svgFillClass={svgFillTheme()}
-								iconType="ts"
-								iconTooltipClass={tooltipTheme()}
-							/>
-						</div>
-					</>
-					<>
-						<h2>Technologies and frameworks</h2>
-						<div className="icon-collection" >
-							<Icon
-								href={"https://reactjs.org/"}
-								svgFillClass={svgFillTheme()}
-								iconType="react"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://nodejs.org/"}
-								svgFillClass={svgFillTheme()}
-								iconType="node"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://expressjs.com/"}
-								svgFillClass={svgFillTheme()}
-								iconType="express"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://www.mongodb.com/"}
-								svgFillClass={svgFillTheme()}
-								iconType="mongodb"
-								iconTooltipClass={tooltipTheme()}
-							/>
-						</div>
-					</>
-					<>
-						<h2>Tools and applications</h2>
-						<div className="icon-collection" >
-							<Icon
-								href={"https://git-scm.com/"}
-								svgFillClass={svgFillTheme()}
-								iconType="git"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://code.visualstudio.com/"}
-								svgFillClass={svgFillTheme()}
-								iconType="vscode"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://www.gimp.org/"}
-								svgFillClass={svgFillTheme()}
-								iconType="gimp"
-								iconTooltipClass={tooltipTheme()}
-							/>
-							<Icon
-								href={"https://www.adobe.com/products/photoshop.html"}
-								svgFillClass={svgFillTheme()}
-								iconType="ps"
-								iconTooltipClass={tooltipTheme()}
-							/>
-						</div>
-					</>
-				</section>
-				<section id="projects">
-					<h1>Projects</h1>
-					<div id="project-index-buttons" style={{ height: (bodyWidth <= 650 ? 48 : 60) }}>
-						<Arrow
-							visibilityClass={projectIndex > 1 ? "visible" : "invisible"}
-							svgStrokeClass={svgStrokeTheme()}
-							posX={(-visibleProjects * (bodyWidth <= 650 ? 230 : 300) / 2)}
-							posY={(bodyWidth <= 650 ? 238 : 297.5)}
-							mirrored={true}
-							clickHandler={handlePrev}
+			<div id="content-body">
+				<main className={isItDark ? "dark-main" : "light-main"} >
+					<section id="welcome">
+						<img
+							id="avatar-image"
+							className={isItDark ? "dark-avatar" : "light-avatar"}
+							src="./images/avatar.png"
+							alt="Akos Varga"
 						/>
-						<Arrow
-							visibilityClass={projectIndex <= NUMBER_OF_PROJECTS - visibleProjects ? "visible" : "invisible"}
-							svgStrokeClass={svgStrokeTheme()}
-							posX={(visibleProjects * (bodyWidth <= 650 ? 230 : 300) / 2)}
-							posY={(bodyWidth <= 650 ? 238 : 297.5)}
-							mirrored={false}
-							clickHandler={handleNext}
-						/>
-					</div>
-					<div
-						id="projects-container"
-						style={{
-							width: (visibleProjects * (bodyWidth <= 650 ? 240 : 300) + (bodyWidth <= 650 ? 60 : 100))
-						}}
-						onTouchStart={handleTouchStart}
-						onTouchMove={handleTouchMove}
-					>
+						<h1 id="topH">
+							Hi! My name is <span>Akoš Varga</span>.
+						</h1>
+						<h2 id="middleH">
+							Welcome to my portfolio page!
+						</h2>
+						<h2>
+							I'm an aspiring <span>front-end developer</span>, constantly improving.
+						</h2>
+					</section>
+					<section id="bio">
+						<h1>About me</h1>
+						<ul>
+							<li>born and living in Serbia</li>
+							<li>self-taught <span>front-end developer</span>, designer</li>
+							<li>possessor of a barely used electronics technician degree</li>
+							<li><span>technology enthusiast</span></li>
+							<li><span>problem solver</span></li>
+							<li>heavy music enjoyer</li>
+							<li>bedroom guitar player</li>
+						</ul>
+					</section>
+					<section id="skills">
+						<h1>My Skills</h1>
+						<>
+							<h2>Languages</h2>
+							<div className="icon-collection" >
+								<Icon
+									href={"https://html.spec.whatwg.org/"}
+									svgFillClass={svgFillTheme()}
+									iconType="html"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://www.w3.org/TR/CSS/"}
+									svgFillClass={svgFillTheme()}
+									iconType="css"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://www.ecma-international.org/publications-and-standards/standards/ecma-262/"}
+									svgFillClass={svgFillTheme()}
+									iconType="js"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://www.typescriptlang.org/"}
+									svgFillClass={svgFillTheme()}
+									iconType="ts"
+									iconTooltipClass={tooltipTheme()}
+								/>
+							</div>
+						</>
+						<>
+							<h2>Technologies and frameworks</h2>
+							<div className="icon-collection" >
+								<Icon
+									href={"https://reactjs.org/"}
+									svgFillClass={svgFillTheme()}
+									iconType="react"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://nodejs.org/"}
+									svgFillClass={svgFillTheme()}
+									iconType="node"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://expressjs.com/"}
+									svgFillClass={svgFillTheme()}
+									iconType="express"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://www.mongodb.com/"}
+									svgFillClass={svgFillTheme()}
+									iconType="mongodb"
+									iconTooltipClass={tooltipTheme()}
+								/>
+							</div>
+						</>
+						<>
+							<h2>Tools and applications</h2>
+							<div className="icon-collection" >
+								<Icon
+									href={"https://git-scm.com/"}
+									svgFillClass={svgFillTheme()}
+									iconType="git"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://code.visualstudio.com/"}
+									svgFillClass={svgFillTheme()}
+									iconType="vscode"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://www.gimp.org/"}
+									svgFillClass={svgFillTheme()}
+									iconType="gimp"
+									iconTooltipClass={tooltipTheme()}
+								/>
+								<Icon
+									href={"https://www.adobe.com/products/photoshop.html"}
+									svgFillClass={svgFillTheme()}
+									iconType="ps"
+									iconTooltipClass={tooltipTheme()}
+								/>
+							</div>
+						</>
+					</section>
+					<section id="projects">
+						<h1>Projects</h1>
+						<div id="project-index-buttons" style={{ height: (bodyWidth <= 650 ? 48 : 60) }}>
+							<Arrow
+								visibilityClass={projectIndex > 1 ? "visible" : "invisible"}
+								svgStrokeClass={svgStrokeTheme()}
+								posX={(-visibleProjects * (bodyWidth <= 650 ? 230 : 300) / 2)}
+								posY={(bodyWidth <= 650 ? 238 : 297.5)}
+								mirrored={true}
+								clickHandler={handlePrev}
+							/>
+							<Arrow
+								visibilityClass={projectIndex <= NUMBER_OF_PROJECTS - visibleProjects ? "visible" : "invisible"}
+								svgStrokeClass={svgStrokeTheme()}
+								posX={(visibleProjects * (bodyWidth <= 650 ? 230 : 300) / 2)}
+								posY={(bodyWidth <= 650 ? 238 : 297.5)}
+								mirrored={false}
+								clickHandler={handleNext}
+							/>
+						</div>
 						<div
-							id="project-slider"
+							id="projects-container"
 							style={{
-								left: ((projectIndex - 1) * (-(bodyWidth <= 650 ? 240 : 300))) + (bodyWidth <= 650 ? 50 : 75)
-							}}>
-							<Project
-								themeClass={projectBoxTheme()}
-								visibilityClass={projectVisibility(1)}
-								title={"Online Teleprompter"}
-								techs={["react", "ts", "css", "html"]}
+								width: (visibleProjects * (bodyWidth <= 650 ? 240 : 300) + (bodyWidth <= 650 ? 60 : 100))
+							}}
+							onTouchStart={handleTouchStart}
+							onTouchMove={handleTouchMove}
+						>
+							<div
+								id="project-slider"
+								style={{
+									left: ((projectIndex - 1) * (-(bodyWidth <= 650 ? 240 : 300))) + (bodyWidth <= 650 ? 50 : 75)
+								}}>
+								<Project
+									themeClass={projectBoxTheme()}
+									visibilityClass={projectVisibility(1)}
+									title={"Online Teleprompter"}
+									techs={["react", "ts", "css", "html"]}
+									svgFillClass={svgFillTheme()}
+									src="./images/project_screenshots/online-teleprompter-screenshot.png"
+									description="The idea has come from the stage teleprompter project, but with much less features. This is the first app I've made with React hooks (useState, useEffect, useRef), and TypeScript. It has a simple user interface, nothing flashy, just a pure learning app. A formula was developed for the ideal text sliding speed after a lot of experiments."
+									buttonThemeClass={projectButtonTheme()}
+									demoHref="https://30isnottoolate.github.io/online-teleprompter-ts"
+									codeHref="https://github.com/30isnottoolate/online-teleprompter-ts"
+								/>
+								<Project
+									themeClass={projectBoxTheme()}
+									visibilityClass={projectVisibility(2)}
+									title={"Stage Teleprompter"}
+									techs={["react", "js", "css", "html"]}
+									svgFillClass={svgFillTheme()}
+									src="./images/project_screenshots/stage-teleprompter-screenshot.png"
+									description="The first project that will actually be used IRL. A musician friend of mine commissioned me to make an app, which will be used in a stage teleprompter box for musicians and public speakers, and controlled with a three-button footswitch. It's made with class components, and will eventually be turned into a desktop app with Electron."
+									buttonThemeClass={projectButtonTheme()}
+									demoHref="https://30isnottoolate.github.io/stage-teleprompter"
+									codeHref="https://github.com/30isnottoolate/stage-teleprompter"
+								/>
+								<Project
+									themeClass={projectBoxTheme()}
+									visibilityClass={projectVisibility(3)}
+									title={"Portfolio Page"}
+									techs={["react", "ts", "css", "html"]}
+									svgFillClass={svgFillTheme()}
+									src="./images/project_screenshots/portfolio-screenshot.png"
+									description="The title says it all. It's my porfolio page. You are currently looking at it. A visual representation of my work, full of SVG images, animations, daring color schemes. Made with React functional components and hooks, written in TypeScript. The projects section is touch swipe compatible. Also, try clicking the gear in the top left corner."
+									buttonThemeClass={projectButtonTheme()}
+									demoHref="https://30isnottoolate.github.io/portfolio"
+									codeHref="https://github.com/30isnottoolate/portfolio"
+								/><Project
+									themeClass={projectBoxTheme()}
+									visibilityClass={projectVisibility(4)}
+									title={"Coming soon..."}
+									techs={["js", "ts", "react"]}
+									svgFillClass={svgFillTheme()}
+									src="https://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-ncs-solid-color-background.jpg"
+									description="Some text here..."
+									buttonThemeClass={projectButtonTheme()}
+									demoHref="https://www.solidbackgrounds.com"
+									codeHref="https://www.solidbackgrounds.com"
+								/><Project
+									themeClass={projectBoxTheme()}
+									visibilityClass={projectVisibility(5)}
+									title={"Coming soon..."}
+									techs={["js", "ts", "react"]}
+									svgFillClass={svgFillTheme()}
+									src="https://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-ncs-solid-color-background.jpg"
+									description="Some text here..."
+									buttonThemeClass={projectButtonTheme()}
+									demoHref="https://www.solidbackgrounds.com"
+									codeHref="https://www.solidbackgrounds.com"
+								/><Project
+									themeClass={projectBoxTheme()}
+									visibilityClass={projectVisibility(6)}
+									title={"Coming soon..."}
+									techs={["js", "ts", "react"]}
+									svgFillClass={svgFillTheme()}
+									src="https://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-ncs-solid-color-background.jpg"
+									description="Some text here..."
+									buttonThemeClass={projectButtonTheme()}
+									demoHref="https://www.solidbackgrounds.com"
+									codeHref="https://www.solidbackgrounds.com"
+								/>
+							</div>
+						</div>
+					</section>
+					<section id="contact">
+						<h1>Get in touch</h1>
+						<div id="contacts">
+							<Contact
 								svgFillClass={svgFillTheme()}
-								src="./images/project_screenshots/online-teleprompter-screenshot.png"
-								description="The idea has come from the stage teleprompter project, but with much less features. This is the first app I've made with React hooks (useState, useEffect, useRef), and TypeScript. It has a simple user interface, nothing flashy, just a pure learning app. A formula was developed for the ideal text sliding speed after a lot of experiments."
-								buttonThemeClass={projectButtonTheme()}
-								demoHref="https://30isnottoolate.github.io/online-teleprompter-ts"
-								codeHref="https://github.com/30isnottoolate/online-teleprompter-ts"
+								contactType={"linkedin"}
 							/>
-							<Project
-								themeClass={projectBoxTheme()}
-								visibilityClass={projectVisibility(2)}
-								title={"Stage Teleprompter"}
-								techs={["react", "js", "css", "html"]}
+							<Contact
 								svgFillClass={svgFillTheme()}
-								src="./images/project_screenshots/stage-teleprompter-screenshot.png"
-								description="The first project that will actually be used IRL. A musician friend of mine commissioned me to make an app, which will be used in a stage teleprompter box for musicians and public speakers, and controlled with a three-button footswitch. It's made with class components, and will eventually be turned into a desktop app with Electron."
-								buttonThemeClass={projectButtonTheme()}
-								demoHref="https://30isnottoolate.github.io/stage-teleprompter"
-								codeHref="https://github.com/30isnottoolate/stage-teleprompter"
+								contactType={"email"}
 							/>
-							<Project
-								themeClass={projectBoxTheme()}
-								visibilityClass={projectVisibility(3)}
-								title={"Portfolio Page"}
-								techs={["react", "ts", "css", "html"]}
+							<Contact
 								svgFillClass={svgFillTheme()}
-								src="./images/project_screenshots/portfolio-screenshot.png"
-								description="The title says it all. It's my porfolio page. You are currently looking at it. A visual representation of my work, full of SVG images, animations, daring color schemes. Made with React functional components and hooks, written in TypeScript. The projects section is touch swipe compatible. Also, try clicking the gear in the top left corner."
-								buttonThemeClass={projectButtonTheme()}
-								demoHref="https://30isnottoolate.github.io/portfolio"
-								codeHref="https://github.com/30isnottoolate/portfolio"
-							/><Project
-								themeClass={projectBoxTheme()}
-								visibilityClass={projectVisibility(4)}
-								title={"Coming soon..."}
-								techs={["js", "ts", "react"]}
-								svgFillClass={svgFillTheme()}
-								src="https://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-ncs-solid-color-background.jpg"
-								description="Some text here..."
-								buttonThemeClass={projectButtonTheme()}
-								demoHref="https://www.solidbackgrounds.com"
-								codeHref="https://www.solidbackgrounds.com"
-							/><Project
-								themeClass={projectBoxTheme()}
-								visibilityClass={projectVisibility(5)}
-								title={"Coming soon..."}
-								techs={["js", "ts", "react"]}
-								svgFillClass={svgFillTheme()}
-								src="https://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-ncs-solid-color-background.jpg"
-								description="Some text here..."
-								buttonThemeClass={projectButtonTheme()}
-								demoHref="https://www.solidbackgrounds.com"
-								codeHref="https://www.solidbackgrounds.com"
-							/><Project
-								themeClass={projectBoxTheme()}
-								visibilityClass={projectVisibility(6)}
-								title={"Coming soon..."}
-								techs={["js", "ts", "react"]}
-								svgFillClass={svgFillTheme()}
-								src="https://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-ncs-solid-color-background.jpg"
-								description="Some text here..."
-								buttonThemeClass={projectButtonTheme()}
-								demoHref="https://www.solidbackgrounds.com"
-								codeHref="https://www.solidbackgrounds.com"
+								contactType={"github"}
 							/>
 						</div>
-					</div>
-				</section>
-				<section id="contact">
-					<h1>Get in touch</h1>
-					<div id="contacts">
-						<Contact
-							svgFillClass={svgFillTheme()}
-							contactType={"linkedin"}
-						/>
-						<Contact
-							svgFillClass={svgFillTheme()}
-							contactType={"email"}
-						/>
-						<Contact
-							svgFillClass={svgFillTheme()}
-							contactType={"github"}
-						/>
-					</div>
-				</section>
-			</main>
-			<footer id="bottom" className={headerFooterTheme()} >© {new Date().getFullYear()} Akos Varga, aka 30isnottoolate</footer>
+					</section>
+				</main>
+				<footer id="bottom" className={headerFooterTheme()} >© {new Date().getFullYear()} Akos Varga, aka 30isnottoolate</footer>
+			</div>
 		</div>
 	);
 }
