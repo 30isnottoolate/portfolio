48000b326b5904a9358a98f97eca91c19a95cb45
30isnottoolate <akos.varga.92@gmail.com>
2022-11-09 16:53:43 +0100

    Remove unnecessary DOM element

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,16 +5,14 @@ interface ContactProps {
     contactType: string;
 }
 
-const Contact: React.FC<ContactProps> = ({ svgFillClass, contactType}: ContactProps) => {
+const Contact: React.FC<ContactProps> = ({ svgFillClass, contactType }: ContactProps) => {
     return (
-        <div>
-            <a className={svgFillClass} href={contacts[contactType as keyof typeof contacts].href} target="_blank" rel="noreferrer">
-                <svg className="contact-image" viewBox="0 0 16 16">
-                    <path d={contacts[contactType as keyof typeof contacts].path} />
-                </svg>
-                <span className="contact-text" >{contacts[contactType as keyof typeof contacts].displayText}</span>
-            </a>
-        </div>
+        <a className={svgFillClass} href={contacts[contactType as keyof typeof contacts].href} target="_blank" rel="noreferrer">
+            <svg className="contact-image" viewBox="0 0 16 16">
+                <path d={contacts[contactType as keyof typeof contacts].path} />
+            </svg>
+            <span className="contact-text" >{contacts[contactType as keyof typeof contacts].displayText}</span>
+        </a>
     );
 }
 
