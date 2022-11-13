c791cce1359cd3d7009b01e3c12c8d89b03f69dc
30isnottoolate <akos.varga.92@gmail.com>
2022-11-13 16:04:37 +0100

    Rework Icon component to fix tooltip issues

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,73 +1,30 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import icons from '../utilities/icons';
 
-const Icon: React.FC<{href: string, iconType: string}> = ({href, iconType}) => {
-    const [tooltipPos, setTooltipPos] = useState({left: 0, top: 0})
-
-    const iconRef = useRef<HTMLAnchorElement>(null);
+const Icon: React.FC<{ href: string, iconType: string }> = ({ href, iconType }) => {
     const tooltipRef = useRef<HTMLParagraphElement>(null);
-
-    useEffect(() => {
-        setTooltipPos({left: getToolTipLeft(), top: getToolTipTop()});
-    }, []);
-
-    useEffect(() => {
-        window.addEventListener("load", () => {
-            setTooltipPos({left: getToolTipLeft(), top: getToolTipTop()});
-        });
-
-        return () => window.removeEventListener("load", () => { });
-    }, []);
-
-    useEffect(() => {
-        window.addEventListener("resize", () => {
-            setTooltipPos({left: getToolTipLeft(), top: getToolTipTop()});
-        });
-
-        return () => window.removeEventListener("resize", () => { });
-    }, []);
-
-    const handleHoverStart = () => {
-        if (tooltipRef.current) tooltipRef.current.style.opacity = "1";
-    }
-
-    const handleHoverEnd = () => {
-        if (tooltipRef.current) tooltipRef.current.style.opacity = "0";
-    }
-
-    const getToolTipLeft = () => {
-        return iconRef.current && tooltipRef.current ? 
-        (iconRef.current.offsetLeft + iconRef.current.offsetWidth / 2 - tooltipRef.current.offsetWidth / 2) : 0;
-    }
-
-    const getToolTipTop = () => {
-        return iconRef.current ? (iconRef.current.offsetTop + iconRef.current.offsetHeight + 5) : 0;
-    }
 
     return (
         <div className="icon-container">
             <a
-                ref={iconRef}
                 href={href}
                 target="_blank"
-                rel="noreferrer" >
+                rel="noreferrer"
+            >
                 <svg
                     className="icon-image svg-fill"
                     viewBox="0 0 128 128"
-                    onMouseEnter={handleHoverStart}
-                    onMouseLeave={handleHoverEnd}
+                    onMouseEnter={() => {if (tooltipRef.current) tooltipRef.current.style.opacity = "1"}}
+                    onMouseLeave={() => {if (tooltipRef.current) tooltipRef.current.style.opacity = "0"}}
                     dangerouslySetInnerHTML={{ __html: icons[iconType as keyof typeof icons].path }}
                 />
             </a>
-            <span
+            <p
                 ref={tooltipRef}
                 className="icon-tooltip"
-                style={{
-                    left: tooltipPos.left,
-                    top: tooltipPos.top
-                }}>
+                >
                 {icons[iconType as keyof typeof icons].iconText}
-            </span>
+            </p>
         </div>
 
     );
