b81df3ece01f01ff126a4be4118e1c1894dce3f1
30isnottoolate <akos.varga.92@gmail.com>
2022-11-16 00:11:46 +0100

    Move delayTimer into useRef value

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, RefObject } from "react"
+import React, { RefObject, useState, useRef, useCallback, useEffect } from "react"
 
 const figures = [
     /*nut*/'<path d="m11.42 2 3.428 6-3.428 6H4.58L1.152 8 4.58 2h6.84zM4.58 1a1 1 0 0 0-.868.504l-3.428 6a1 1 0 0 0 0 .992l3.428 6A1 1 0 0 0 4.58 15h6.84a1 1 0 0 0 .868-.504l3.429-6a1 1 0 0 0 0-.992l-3.429-6A1 1 0 0 0 11.42 1H4.58z"/><path d="M6.848 5.933a2.5 2.5 0 1 0 2.5 4.33 2.5 2.5 0 0 0-2.5-4.33zm-1.78 3.915a3.5 3.5 0 1 1 6.061-3.5 3.5 3.5 0 0 1-6.062 3.5z"/>',
@@ -11,7 +11,7 @@ const figures = [
 const Background: React.FC<{ givenID: string, contentRef: RefObject<HTMLDivElement> }> = ({ givenID, contentRef }) => {
     const [wallpaper, setWallpaper] = useState("");
 
-    let delayTimer: ReturnType<typeof setTimeout>;
+    const delayTimer = useRef(setTimeout(() => { }));
 
     const generateBackground = (contentWidth: number, contentHeight: number) => {
         let backgroundContent = "";
@@ -33,9 +33,9 @@ const Background: React.FC<{ givenID: string, contentRef: RefObject<HTMLDivEleme
     }
 
     const updateBackground = useCallback(() => {
-        if (delayTimer) clearTimeout(delayTimer);
+        clearTimeout(delayTimer.current);
 
-        delayTimer = setTimeout(() => {
+        delayTimer.current = setTimeout(() => {
             if (contentRef.current) setWallpaper(generateBackground(contentRef.current.offsetWidth, contentRef.current.offsetHeight));
         }, 500);
     }, [contentRef]);
