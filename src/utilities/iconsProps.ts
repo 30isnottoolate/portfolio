bf4b6a2eb3f1f2b367eb1b56d698d01b8a2074de
30isnottoolate <akos.varga.92@gmail.com>
2023-01-10 12:12:22 +0100

    Create iconsProps utility

---
+++ src/utilities/iconsProps.ts
@@ -0,0 +1,15 @@
+const iconsProps = [
+    { href: "https://html.spec.whatwg.org/", iconType: "html" },
+    { href: "https://www.w3.org/TR/CSS/", iconType: "css" },
+    { href: "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/", iconType: "js" },
+    { href: "https://www.typescriptlang.org/", iconType: "ts" },
+    { href: "https://reactjs.org/", iconType: "react" },
+    { href: "https://nodejs.org/", iconType: "node" },
+    { href: "https://expressjs.com/", iconType: "express" },
+    { href: "https://www.mongodb.com/", iconType: "mongodb" },
+    { href: "https://git-scm.com/", iconType: "git" },
+    { href: "https://code.visualstudio.com/", iconType: "vscode" },
+    { href: "https://www.gimp.org/", iconType: "gimp" },
+    { href: "https://www.adobe.com/products/photoshop.html", iconType: "ps" },]
+
+export default iconsProps;
