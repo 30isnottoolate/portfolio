bbdb37f3215d541e37ce2f38005a6dd101016656
30isnottoolate <akos.varga.92@gmail.com>
2023-01-15 14:26:31 +0100

    Change menu-button-parts translate values to rem

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -8,9 +8,9 @@ interface MenuButtonProps {
 const MenuButton: React.FC<MenuButtonProps> = ({clickHandler, isMenuVisible}: MenuButtonProps) => {
     return (
         <div id="menu-button" style={{cursor: "pointer"}} onClick={clickHandler} >
-            <div className="menu-button-parts" style={{transform: (isMenuVisible ? "translate(0, 10px) rotate(-45deg)" : "none")}} />
+            <div className="menu-button-parts" style={{transform: (isMenuVisible ? "translate(0, 0.5rem) rotate(-45deg)" : "none")}} />
             <div className="menu-button-parts" style={{opacity: (isMenuVisible ? "0" : "1")}}/>
-            <div className="menu-button-parts" style={{transform: (isMenuVisible ? "translate(0, -10px) rotate(45deg)" : "none")}}/>
+            <div className="menu-button-parts" style={{transform: (isMenuVisible ? "translate(0, -0.5rem) rotate(45deg)" : "none")}}/>
         </div>
     );
 }
