8e4f3741811f8e40768dabf63b1463542017a960
30isnottoolate <akos.varga.92@gmail.com>
2023-01-15 21:52:15 +0100

    Add wideMode, microMode states to component
        These states track screen width to make NavBar more responsive.
        Added conditional changing of header height.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import MenuButton from './MenuButton';
 
 const NavBar: React.FC = () => {
     const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [wideMode, setWideMode] = useState(true);
+    const [microMode, setMicroMode] = useState(false);
 
     useEffect(() => {
         setIsMenuVisible(document.body.offsetWidth <= 650 ? false : true);
+        setWideMode(document.body.offsetWidth > 650 ? true : false);
+        setMicroMode(document.body.offsetWidth <= 380 ? true : false);
     }, [])
 
     useEffect(() => {
-		window.addEventListener("resize", () => {
-			setIsMenuVisible(document.body.offsetWidth <= 650 ? false : true);
-		});
-		return () => window.removeEventListener("resize", () => { });
-	}, []);
+        window.addEventListener("resize", () => {
+            setIsMenuVisible(document.body.offsetWidth <= 650 ? false : true);
+            setWideMode(document.body.offsetWidth > 650 ? true : false);
+            setMicroMode(document.body.offsetWidth <= 380 ? true : false);
+        });
+        return () => window.removeEventListener("resize", () => { });
+    }, []);
 
     const handleMenuClick = () => setIsMenuVisible((prevValue) => !prevValue);
 
     return (
-        <header className="header-footer" >
+        <header
+            className="header-footer"
+            style={{
+                height: `${
+                    wideMode ? "3.5rem" :
+                    !wideMode && !microMode && !isMenuVisible ? "2.75rem" :
+                    !wideMode && !microMode && isMenuVisible ? "5.5rem" :
+                    microMode && !isMenuVisible ? "2.75rem" : "13.75rem"}`,
+                borderRadius: `0 0 0 ${isMenuVisible && !wideMode ? "0" :
+                    !isMenuVisible && !wideMode ? "1.25rem" : "1.75rem"}`
+            }} >
             <a
                 id="logo"
                 href="#welcome" >
