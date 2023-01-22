515d796b96c02f7b005c83b0ac264e36c893b020
30isnottoolate <akos.varga.92@gmail.com>
2023-01-22 18:02:05 +0100

    Create projects utility for storing projects

---
+++ src/utilities/projects.tsx
@@ -0,0 +1,52 @@
+const projects = [
+    {
+        title: "Stage Teleprompter",
+        techs: ["react", "ts", "css", "html"],
+        src: "./images/project_screenshots/stage-teleprompter-screenshot.png",
+        description: <>A musician friend of mine commissioned me to make this app, which will be used in a stage teleprompter for musicians and public speakers. My first project that is actually being used IRL. Originally made with class components, now rewritten with React hooks.</>,
+        demoHref: "https://30isnottoolate.github.io/stage-teleprompter-hooked-ts",
+        codeHref: "https://github.com/30isnottoolate/stage-teleprompter-hooked-ts"
+    },
+    {
+        title: "Library Manager",
+        techs: ["react", "ts", "css", "html"],
+        src: "./images/project_screenshots/teleprompter-library-manager-screenshot.png",
+        description: <>This is a utility application for the Stage Teleprompter project. You can create texts, format them, highlight selected parts with various colors, and change their order to your liking. <br/>It will eventually be turned into a multiplatform application with Electron.</>,
+        demoHref: "https://30isnottoolate.github.io/teleprompter-library-manager",
+        codeHref: "https://github.com/30isnottoolate/teleprompter-library-manager"
+    },
+    {
+        title: "Portfolio Page",
+        techs: ["react", "ts", "css", "html"],
+        src: "./images/project_screenshots/portfolio-screenshot.png",
+        description: <>The title says it all. It's my porfolio page. You are looking at it. A visual portrayal of my work, full of SVG images, animations, daring color schemes and a swipeable projects section. It's made with React hooks and functional components in TypeScript.</>,
+        demoHref: "https://30isnottoolate.github.io/portfolio",
+        codeHref: "https://github.com/30isnottoolate/portfolio"
+    },
+    {
+        title: "Online Teleprompter",
+        techs: ["react", "ts", "css", "html"],
+        src: "./images/project_screenshots/online-teleprompter-screenshot.png",
+        description: <>The idea has come from the Stage Teleprompter project, but this one has less features. It is the first app I've made with React hooks and TypeScript. It has a simple, easy-to-use UI. <br/>A formula was developed for the ideal text sliding speed after a lot of experiments.</>,
+        demoHref: "https://30isnottoolate.github.io/online-teleprompter-ts",
+        codeHref: "https://github.com/30isnottoolate/online-teleprompter-ts"
+    },
+    {
+        title: "Coming soon...",
+        techs: ["js", "ts", "react"],
+        src: "https://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-ncs-solid-color-background.jpg",
+        description: <>Some text here...</>,
+        demoHref: "https://www.solidbackgrounds.com",
+        codeHref: "https://www.solidbackgrounds.com"
+    },
+    {
+        title: "",
+        techs: ["js", "ts", "react"],
+        src: "https://www.solidbackgrounds.com/images/1920x1080/1920x1080-red-ncs-solid-color-background.jpg",
+        description: <>Some text here...</>,
+        demoHref: "https://www.solidbackgrounds.com",
+        codeHref: "https://www.solidbackgrounds.com"
+    },
+]
+
+export default projects;
